26726a7f6ecf94f6224718c86f5a5a8736ea016b
EJH506 <djawlgp456@gmail.com>
2025-01-06 15:34:10 +0900

    메인화면의 게시글목록에 삭제된 글 미노출하도록 수정

--- src/main/java/index/IndexMgr.java
+++ src/main/java/index/IndexMgr.java
@@ -30,7 +30,7 @@ public class IndexMgr {
 		
 		try {
 			conn = pool.getConnection();
-			sql = "select nickname,content,boardid from boardtbl order by boardid desc limit 10";
+			sql = "select nickname,content,boardid from boardtbl where status = 0 order by boardid desc limit 10";
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
 			
@@ -62,7 +62,7 @@ public class IndexMgr {
 		
 		try {
 			conn = pool.getConnection();
-			sql = "select nickname,content,boardid from boardtbl where best = 'Y' order by regdate limit 10";
+			sql = "select nickname,content,boardid from boardtbl where best = 'Y' and status = 0 order by regdate limit 10";
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
 			
